abd065c925d9fa50f139be71051676cfe2bcb040
Samŭ <samuelaure@gmail.com>
2023-02-10 08:49:22 +0100

    [Solved] TypeScript/space-age

---
+++ typescript/space-age/space-age.ts
@@ -0,0 +1,18 @@
+interface PeriodMap {
+  [key : string]: number
+}
+
+const planetsYears: PeriodMap = {
+  mercury: 0.2408467,
+  venus: 0.61519726,
+  earth: 1.0,
+  mars: 1.8808158,
+  jupiter: 11.862615,
+  saturn: 29.447498,
+  uranus: 84.016846,
+  neptune: 164.79132
+}
+
+export function age(planet: string, seconds: number): number {
+  return Number((seconds / (planetsYears[planet] * 31557600)).toFixed(2))
+}
